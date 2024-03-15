e0658e76c7a5dabca2b3ee37d02017d8153172df
https://github.com/AytbaevErlan <irlanaytbaev@gmail.com>
2024-03-15 02:34:32 +0100

    updates

--- src/main/java/com/code/springcoredemo/rest/DemoController.java
+++ src/main/java/com/code/springcoredemo/rest/DemoController.java
@@ -13,7 +13,9 @@ public class DemoController {
     private Coach myCoach;
 
     @Autowired
-    public DemoController(@Qualifier("cricketCoach") Coach theCoach){
+    public DemoController(
+            @Qualifier("cricketCoach") Coach theCoach){
+        System.out.println("In constructor: " + getClass().getSimpleName());
         myCoach = theCoach;
     }
 
