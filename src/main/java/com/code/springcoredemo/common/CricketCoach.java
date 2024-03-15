6d9190026e6289c278a8e1de2929707cc026f32e
https://github.com/AytbaevErlan <irlanaytbaev@gmail.com>
2024-03-15 19:57:36 +0100

    updates

--- src/main/java/com/code/springcoredemo/common/CricketCoach.java
+++ src/main/java/com/code/springcoredemo/common/CricketCoach.java
@@ -1,8 +1,6 @@
 package com.code.springcoredemo.common;
 
 
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,17 +10,6 @@ public class CricketCoach implements Coach{
         System.out.println("In constructor: " + getClass().getSimpleName());
     }
 
-    //define  init method
-    @PostConstruct
-    public void doMyStartUpStuff(){
-        System.out.println("In doMyStartUpStuff(): " + getClass().getSimpleName());
-    }
-
-    //define destroy method
-    @PreDestroy
-    public void doMyCleanUpStuff(){
-        System.out.println("In doMyCleanUpStuff(): " + getClass().getSimpleName());
-    }
     @Override
     public String getDailyWorkout() {
         return "Practice fast bowling for 15 minutes";
---
+++ src/main/java/com/code/springcoredemo/config/SportConfig.java
@@ -0,0 +1,15 @@
+package com.code.springcoredemo.config;
+
+import com.code.springcoredemo.common.Coach;
+import com.code.springcoredemo.common.SwimCoach;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SportConfig {
+
+    @Bean("aquatic")
+    public Coach swimCoach(){
+        return new SwimCoach();
+    }
+}
